b37ea71748311b79fcf082c005f6e81dab174351
Meekun <orofinadedamola@gmail.com>
2022-08-25 02:12:36 +0100

    :art: Customize Chakra-UI Components

---
+++ theme/components/heading.ts
@@ -0,0 +1,44 @@
+import type { ComponentStyleConfig } from '@chakra-ui/theme'
+
+const Heading: ComponentStyleConfig = {
+    baseStyle: { 
+        lineHeight: 7,
+    },
+
+    variants: {
+        'logo-heading': {
+            lineHeight: 10,
+            textTransform: 'uppercase',
+            color: 'brand.300',
+            fontWeight: 'bold',
+            fontSize: '1.6rem',
+        },
+        'h2': {
+            fontWeight: 'bold',
+            fontSize: '2xl',
+            lineHeight: 7,
+        }, 
+        'h3': {
+            fontSize: '2xl',
+            fontWeight: 'bold',
+            letterSpacing:'wide',
+            lineHeight: 7,
+            pb: 1.5,
+        }, 
+        'h4': {
+            fontSize: '2xl',
+            fontWeight: 'semibold',
+            textAlign: 'center',
+            lineHeight: 7,
+            mb: 8,
+        },
+        'footer-heading': {
+            fontSize: 'xl',
+            color: 'brand.300',
+            fontWeight: 'bold',
+            mb: 4,
+        }
+    }
+}
+
+export default Heading
