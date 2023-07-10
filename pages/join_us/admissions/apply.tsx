3bff79d68f0d6dbad8233c1226227f7c18584313
chelcee-xx <chelcee1614@gmail.com>
2023-07-10 14:44:50 +0100

    Removed visit THS button

---
+++ pages/join_us/admissions/apply.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+import type { NextPage } from "next"
+import Apply from "../../../components/pages/Apply"
+
+const Applypage: NextPage = () => {
+	return (
+		<>
+			<Apply />
+		</>
+	)
+}
+
+export default Applypage
